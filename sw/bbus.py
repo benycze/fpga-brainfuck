ba3482036118e860d5a8afc1bf7443dc4984a774
Pavel Benacek <pavel.benacek@gmail.com>
2020-06-28 19:16:40 +0200

    bbus tool added for easier handling with data via the UART.

---
+++ sw/bbus.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python3
+
+# -------------------------------------------------------------------------------
+#  PROJECT: FPGA Brainfuck
+# -------------------------------------------------------------------------------
+#  AUTHORS: Pavel Benacek <pavel.benacek@gmail.com>
+#  LICENSE: The MIT License (MIT), please read LICENSE file
+#  WEBSITE: https://github.com/benycze/fpga-brainfuck/
+# -------------------------------------------------------------------------------
+
+import brainfuck_io.io as bio
+import pdb
+import sys
+import argparse
+import random
+import signal
+import time
+
+
+# Number of suc. tests after which we print the '+' character
+TEST_DOT_CNT = 100
+# Flag to stop testing
+TEST_EN = True
+
+def interupt_signal_handler(sig, frame):
+    """
+    Interupt signal handler
+    """
+    global TEST_EN
+    TEST_EN = False
+
+def get_parser(args):
+    """
+    Return the parser of arguments
+
+    Parameters:
+        - args - arguments to parse
+    """
+    # Remove the leading app path
+    prgname = args[0]
+    args = args[1:]
+
+    parser = argparse.ArgumentParser(description='Brainfuck bus program which allows you to read/write 8-bit data'
+    'chunks from the FPGA device. Brief information how to use the command: \n\n'
+    '   * Read data from address 0x22 - {0} 0x22 \n'
+    '   * Write data 0x1 to address 0x33 - {0} 0x33 0x1 \n'
+    'You are also allowed to send more bytes at once where the passed address will be used for the first byte (from LSB) and following bytes '
+    'will be written on incremented address.'.format(prgname),formatter_class=argparse.RawTextHelpFormatter)
+
+    parser.add_argument('--device',type=str,nargs=1,help='Specify the path to the device.',default='/dev/ttyUSB0')
+    parser.add_argument('--test',type=int,nargs=1,help='Run the infinite r/w test until the CTRL+C is fired. The passed argument is the address space bit width.')
+    parser.add_argument('command',type=lambda x: int(x,0),nargs='*',help='There are two possible commands - read and write.'
+    'Read is invoked iff only address is passed. Write is invoked iff we pass additonal value argument.')
+
+    return parser.parse_args(args)
+
+def int_to_bytes(data):
+    """
+    Convert passed data (int) to bytes
+
+    Return: list of bytes
+    """
+    # Check if the passed data are 0x0, if yes, return the array with byte representation
+    if(data == 0x0):
+        return [b'\x00']
+
+    # Byte coversion is done via the cycle, where the process
+    # is repeated until the data are equal to 0. Everything is taken in 
+    # the little-endian format
+    tmp_data = data
+    ret = []
+    while tmp_data != 0:
+        dt = tmp_data & 0xff
+        tmp_data = tmp_data >> 8
+        con = dt.to_bytes(1,byteorder='little')
+        ret.append(con)
+    return ret
+
+def print_byte(data):
+    """
+    Print passed data to the output
+    """
+    # Print the HEX format of read data
+    conv_data = hex(int.from_bytes(data,byteorder='little'))
+    print(conv_data)
+
+def start_test(dev,awidth):
+    """
+    Start the test of address space
+
+    Parameters:
+        - dev - device to work with
+        - awidth - address space width which will be tested
+    """
+    print("Test mode has been detected. The rest of the command is ignored.\n")
+    print(" * Tested address space => {}".format(awidth))
+    print(" * Test prints the '+' characted after every {}".format(TEST_DOT_CNT))
+    print(" * USE CTRL + C to stop the testing process.\n\n")
+
+    signal.signal(signal.SIGINT, interupt_signal_handler)
+    tmp_addr    = 0x0
+    tmp_data    = []
+    succ_test   = 0
+    start_time  = int(time.time())
+    max_addr    = (2**awidth)-1
+
+    while TEST_EN:
+        # Generate random data
+        rnd_input = random.randint(0,255)
+        tmp_data = int_to_bytes(rnd_input)
+        print(hex(rnd_input))
+        # Write & read the data
+        write(dev,tmp_addr,tmp_data)
+        tmp_read = read(dev,tmp_addr)
+        # Check the result
+        conv_read = int.from_bytes(tmp_read,byteorder='little')
+        if(conv_read == rnd_input):
+            # Read data are same, increment the counter and print the progress
+            succ_test = succ_test + 1
+            if succ_test % TEST_DOT_CNT == 0:
+                print("+",end='',flush=True)
+        else:
+            # Invalid test, report it and return the value
+            print("\n\n INVALID TEST ==>\n")
+            print("Address: {}".format(hex(tmp_addr)))
+            print("Expected data: {}".format(hex(rnd_input)))
+            print("Received data: {}".format(hex(conv_read)))
+            break
+
+        # Prepare the next iteration, we need to reset the 
+        # address iff we reached the maximal one 
+        if tmp_addr == max_addr:
+            tmp_addr = 0
+        else:
+            tmp_addr = tmp_addr + 1
+
+    # Print some statistics about testing
+    stop_time   = int(time.time())
+    diff        = stop_time - start_time
+    mins,sec    = divmod(diff,60)
+    hour,mins   = divmod(mins,60)
+    operations_per_sec = float(succ_test)/float(diff)
+
+    print("Performed tests: {}".format(succ_test))
+    print("Operations/sec: {}".format(operations_per_sec))
+    print("Runtime: {}h {}m {}s".format(int(hour),int(mins),int(sec)))
+
+def write(dev,addr,data):
+    """
+    Write data to FPGA
+
+    Parameters:
+        - dev - device to work with
+        - addr - address to write to
+        - data - data to write 
+    """
+    tmp_addr = addr
+    for b in data:
+        dev.write(tmp_addr,b)
+        tmp_addr = tmp_addr + 1
+
+def read(dev,addr):
+    """
+    Read data from the FPGA
+
+    Parameters:
+        - addr - address to read from
+    """
+    return dev.read(addr)
+
+
+def process(args,dev):
+    """
+    Process commands and start the required operation
+    """
+    # The test command has biggere precende
+    if(args.test is not None):
+        start_test(dev,args.test[0])
+    elif(len(args.command) == 1):
+        # Read command asserted
+        addr = args.command[0]
+        rd = read(dev,addr)
+        print_byte(rd)
+    elif(len(args.command) == 2):
+        # Write command asserted
+        addr = args.command[0]
+        data = int_to_bytes(args.command[1])
+        write(dev,addr,data)
+    else:
+        print("Invalid command, see --help for more details.")
+
+def main():
+    """
+    Main entry function
+    """
+    # Parse arguments
+    args = get_parser(sys.argv) 
+    # Start the main body of the program
+    dev = None
+    try:
+        dev = bio.BrainfuckIO(args.device)
+        process(args,dev)
+    except Exception as e:
+        print("Error during the program processing : \n\n",file=sys.stderr)
+        print(str(e),file=sys.stderr)
+    finally:
+        if dev is None:
+            dev.close()
+
+if __name__ == "__main__":
+    main()
