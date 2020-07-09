60a201a328b5b30d2095c8539aa0ea4c34a1603f
Pavel Benacek <pavel.benacek@gmail.com>
2020-07-09 22:10:11 +0200

    Script, makefile and README were update to provide a simple way of the translation.

--- bsv/start-container.sh
+++ bsv/start-container.sh
@@ -8,13 +8,62 @@
 #  WEBSITE: https://github.com/benycze/fpga-brainfuck/
 #--------------------------------------------------------------------------------
 
-echo "############################################################################"
-echo "Starting the docker container with BSC tools "
-echo "############################################################################"
+set -e
 
-echo " * We are looking for the bsc-compiler image"
-echo " * Local folder will be mounted to /bsc-work folder inside the container :)"
-
+# Configuration -----------------------------------------------------------------
 MNT_PATH=`pwd`
 MNT_WDIR="/bsc-work"
-docker run --rm -t -i --mount=type=bind,source=${MNT_PATH},destination=${MNT_WDIR} --workdir=${MNT_WDIR} localhost/bsc-compiler bash
+
+DOCKER_BIN=/usr/bin/docker
+
+# Functions & code --------------------------------------------------------------
+
+function print_usage {
+    echo "The script has following commands:"
+    echo "-t or --translate     => run the translation, output will be in the tarball folder"
+    echo "-h or --help          => prints the HELP"
+    echo ""
+    echo "The interactive mode is started if you don't pass any argument."
+}
+
+function start_docker {
+    echo "############################################################################"
+    echo "Starting the docker container with BSC tools "
+    echo "############################################################################"
+
+    echo " * We are looking for the bsc-compiler image"
+    echo " * Local folder will be mounted to /bsc-work folder inside the container :)"
+
+    ${DOCKER_BIN} run --rm -t -i --mount=type=bind,source=${MNT_PATH},destination=${MNT_WDIR} --workdir=${MNT_WDIR} localhost/bsc-compiler $1
+}
+
+# Parse arguments ----------------------------------------------------------------
+translate_only=0
+
+while [ "$1" != "" ]; do
+    case "$1" in
+        "-t" | "--translate") 
+            translate_only=1
+            ;;
+
+        "-h" | "--help") 
+            print_usage
+            exit 0
+            ;;
+
+        *)  echo -n "Unknown parameter!!\n"
+            print_usage
+            exit 1
+            ;;
+    esac  
+    shift
+done
+
+# Run the code ------------------------------------------------------------------
+if [ $translate_only -eq 1 ];then
+    start_docker "make tarball"
+else
+    start_docker bash
+fi
+
+exit 0
